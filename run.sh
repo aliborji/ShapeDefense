eeafc17fe7f6fb6c406120ef829b32a3ba3f0d3a
Your Name <aliborji@gmail.com>
2020-08-27 19:32:34 -0400

    sample res

--- run.sh
+++ run.sh
@@ -42,3 +42,23 @@ python test_noise_robustness.py --n rgbedge -m FGSM-Icons/icons_rgbedge_8_robust
 python test_noise_robustness.py --n edge -m FGSM-Icons/icons_edge_32_robust_32.pth --data_dir Icons-50 --classes 50 --inp_size 64
 python test_noise_robustness.py --n rgb -m FGSM-Icons/icons_rgb_32_robust_32.pth --data_dir Icons-50 --classes 50 --inp_size 64
 python test_noise_robustness.py --n rgbedge -m FGSM-Icons/icons_rgbedge_32_robust_32.pth --data_dir Icons-50 --classes 50 --inp_size 64
+
+
+
+
+# some samples
+# for training pix to pix
+# python train_adv_gan.py --net_type gray --model mnist  --sigmas 8 32 --data_dir mnist --classes 10 --epochs 1 --inp_size 28 --gan_model netG_model_epoch_10.pth
+
+# Training a robust classifier from the generated samples
+# cd pix2pix-pytorch
+# python train.py --niter 2 --niter_decay 1 --input_nc 1 --output_nc 1 --ngf 10 --ndf 10 --dataset mnist
+# and testing it
+# python test.py --dataset cifar10 --nepochs 60 --inp_size 32
+# [you need to modify the ganutils.py file accordingly]
+
+
+
+
+
+
